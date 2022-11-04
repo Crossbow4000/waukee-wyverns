34638f7abe04b056cdcf7a80dc6e3f245583d62b
Crossbow4000 <rvennapusa4000@gmail.com>
2022-11-03 19:15:25 -0500

    Stopped scrolling when menu is open

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const primaryNav = document.querySelector(".navigation");
 const navToggle = document.querySelector(".nav-toggle");
+const body = document.querySelector(".body");
 
 navToggle.addEventListener("click", () =>{
     const visibility = primaryNav.getAttribute("data-visible")
@@ -7,9 +8,11 @@ navToggle.addEventListener("click", () =>{
     if (visibility === "false"){
         primaryNav.setAttribute("data-visible", true)
         navToggle.setAttribute("data-fixed", true)
+        body.setAttribute("data-noscroll", true)
     }
     else if (visibility === "true"){
         primaryNav.setAttribute("data-visible", false)
         navToggle.setAttribute("data-fixed", false)
+        body.setAttribute("data-noscroll", false)
     }
 })
