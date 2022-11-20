8d71ad2309789a8d7988c3eef8ac01381513d2ef
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2022-11-20 10:42:04 -0600

    Update get-data.js

--- js/get-data.js
+++ js/get-data.js
@@ -1,6 +1,5 @@
-const api_url = "https://script.googleusercontent.com/macros/echo?user_content_key=t_3rB0qYWTQThCmTQX1OQ1yBsV9X7pSs73MTxVwotFihYvdGhrrsDTUFz-KKDyFfVOcl_sKs1W0aajUVRbKhaSkHy2RR6WUrm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJHuS9z0YuhrT-3Vq260P0SEZjS-CAk89NyabIMq29l37BewtScA0vkHXISjUnmfNKT-onlEH2COoNZVO3cHtXkMxjqVWQYRkg&lib=MN6OZnxKwUW0oF-2LQ9t5J1IOmpMGpgTGc";
-let template = {'<>':'div','html':'${userid} ${id}'};
+const api_url = 'https://api.sheety.co/5ee27d6536c5a1b1bac026b071e23292/apiStuff/sheet1';
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+fetch(api_url)
   .then(response => alert(response))
-  .then(json => lele)
+  .then(json => console.log(json))
