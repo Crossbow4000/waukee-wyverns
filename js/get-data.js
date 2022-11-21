c38b732b1cc98298cd591bba7069784d540671f8
Crossbow4000 <rvennapusa4000@gmail.com>
2022-11-20 19:17:05 -0600

    changed some stuff

--- js/get-data.js
+++ js/get-data.js
@@ -1,5 +1,5 @@
 const api_url = 'https://api.sheety.co/5ee27d6536c5a1b1bac026b071e23292/apiStuff/sheet1';
 
 fetch(api_url)
-  .then(response => alert(response))
+  .then(response => json)
   .then(json => console.log(json))
